970aa1a3a3809f63e481ef5c4bc87df696d3dc67
sorianomATWIT <sorianom@wit.edu>
2021-03-09 11:23:38 -0500

    wow

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
