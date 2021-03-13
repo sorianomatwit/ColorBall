4d89ccf5fd103f650531b29f796da1331bb53992
sorianomATWIT <sorianom@wit.edu>
2021-03-13 07:09:49 -0500

    3/13

--- src/ColorPicker.java
+++ src/ColorPicker.java
@@ -2,50 +2,61 @@ import java.util.ArrayList;
 import java.util.Random;
 
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
-public abstract class ColorPicker {
-	
+public class ColorPicker {
+
 	private Random rand;
-	//list of colors to choose from
-	protected Color[] ColorList = {Color.AQUA,Color.HOTPINK,Color.PURPLE,
-			Color.LIGHTGRAY,Color.LIME,Color.RED,Color.ORANGE,Color.YELLOW};
-	
+	// list of colors to choose from
+	protected Color[] ColorList = { Color.AQUA, Color.HOTPINK, Color.PURPLE, Color.LIGHTGRAY, Color.LIME, Color.RED,
+			Color.ORANGE, Color.YELLOW };
+
 	// colors of which will be used
 	ArrayList<Color> chosenColors = new ArrayList<Color>();
-	
-	//numbers of colors used
+
+	// numbers of colors used
 	protected int numOfColors;
-	
+
 	protected ColorPicker(int numof) {
-		//initialize random
+		// initialize random
 		rand = new Random();
 		numOfColors = numof;
-		//select colors
+		// select colors
 		reSelect();
 	}
-	
-	
+
 	/**
-	 * add colors to chosencolors in order to know what 
-	 * colors to use and select colors at random
+	 * add colors to chosencolors in order to know what colors to use and select
+	 * colors at random
 	 */
 	protected void reSelect() {
 		chosenColors.clear();
-		for(int i = 0;i < numOfColors;i++) {
+		for (int i = 0; i < numOfColors; i++) {
 			chosenColors.add(ColorList[rand.nextInt(ColorList.length)]);
 		}
+		for (int i = 0; i < chosenColors.size(); i++) {
+			Color c = chosenColors.get(i);
+			for (int k = 0; k < chosenColors.size(); k++) {
+				Color c1 = chosenColors.get(k);
+				if (c.equals(c1) && i != k) {
+					chosenColors.remove(k);
+					chosenColors.add(ColorList[rand.nextInt(ColorList.length)]);
+				}
+			}
+		}
 	}
+
 	/**
 	 * wanting to reset the amount of colros used and pick new colors
+	 * 
 	 * @param newNumOf number of colors you want to use
 	 */
 	protected void reSelect(int newNumOf) {
 		chosenColors.clear();
 		numOfColors = newNumOf;
-		for(int i = 0;i < numOfColors;i++) {
+		for (int i = 0; i < numOfColors; i++) {
 			chosenColors.add(ColorList[rand.nextInt(ColorList.length)]);
 		}
 	}
-	
-	
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,22 @@
+import java.util.ArrayList;
+
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 import javafx.util.Duration;
 
 public class Main extends Application {
-
+	
+	int game_diff = 1;
 	public static void main(String[] args) {
 		launch(args);
 
@@ -18,25 +24,41 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		Wall w = new Wall();
+		
+		ColorPicker c = new ColorPicker(7);
+		ArrayList<Wallpiece> wall = new ArrayList<Wallpiece>();
+		for(int i = 0; i < c.numOfColors;i++) {
+			wall.add(new Wallpiece(c.chosenColors.get(i),i,100));
+		}
+		
+		Rectangle r = new Rectangle(100,100,100,100);
+		r.setFill(c.chosenColors.get(0));
 		
 		
 		Pane pane = new Pane();
 		
-		
+		pane.getChildren().addAll(wall);
 		Scene scene = new Scene(pane,500,500);
 		primaryStage.setTitle("Platformer");
 		primaryStage.setScene(scene);
 		primaryStage.show();
 		
-		w.CreateWall(scene);
+		double heights = scene.getHeight()/c.numOfColors;
+		for(int i = 0; i < wall.size();i++) {
+			Wallpiece w = wall.get(i);
+			w.Update(scene.getWidth(),(0+ i*heights),heights);
+		}
+		
+		
 		//this well happen every frame
 		EventHandler<ActionEvent> step = new EventHandler<ActionEvent>() {
 
 			@Override
 			public void handle(ActionEvent arg0) {
-				// TODO Auto-generated method stub
-				
+				//everything here hapopens every frame
+				for(Wallpiece w: wall) {
+					w.display();
+				}
 			}
 			
 		};
